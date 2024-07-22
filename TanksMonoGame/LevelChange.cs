35a7f375797f80517d14b782e03c0c42098e2384
David Jansons <51482006+djansons@users.noreply.github.com>
2024-07-22 14:15:33 +1000

    Update LevelChange.cs

--- TanksMonoGame/LevelChange.cs
+++ TanksMonoGame/LevelChange.cs
@@ -23,7 +23,7 @@ namespace Tanks
 
         Rectangle menuRect = new Rectangle(230, -900, 300, 800);
         Vector2 fontNewGamePos = new Vector2(337, 265);
-        Vector2 fontHighScorePos = new Vector2(334, 345);
+        Vector2 fontHighScorePos = new Vector2(3s34, 345);
         Vector2 fontHelpPos = new Vector2(360, 425);
         Vector2 fontExitPos = new Vector2(360, 500);
 
